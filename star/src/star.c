7259c62438ca6666e8fef6d44b462c234554ac82
sungbin <21700613@handong.edu>
2022-03-17 09:59:16 +0900

    star list()

--- star/src/star.c
+++ star/src/star.c
@@ -12,11 +12,16 @@
 
 #include "../include/star.h"
 
-unsigned int file_no = 0;
+int
+mkdirs(char * dir_path);
+
+char * parent_dir(char * dir_path);
+
+int file_no = 0;
 unsigned int data_size = 0;
 
 void
-archive_path (char * target_dir, char * des_dir, char * sub_dir, FILE * p_fp, FILE * d_fp);
+archive_path (char * target_dir, char * des_dir, char * sub_dir, FILE * s_fp);
 
 void
 archive (char * target_dir, char * star_path) {
@@ -33,50 +38,15 @@ archive (char * target_dir, char * star_path) {
 	*/
 
 	FILE * star_fp = fopen(star_path, "wb");
-
-	char * path_name = ".path";
-	FILE * p_fp = fopen(path_name, "wb");
-	char * data_name = ".data";
-	FILE * d_fp = fopen(data_name, "wb");
-
-
-	archive_path(target_dir, basename(target_dir), "", p_fp, d_fp);
-	fclose(p_fp);
-	fclose(d_fp);
-
-	int buf_size = fwrite(&file_no, 1, 4, star_fp);
-	
-	p_fp = fopen(path_name, "rb");
-	buf_size = 0;
-	do {
-		char buf[512];
-		buf_size = fread(buf, 1, 512, p_fp);
-		fwrite(buf, 1, buf_size, star_fp);
-
-	} while(buf_size > 0);
-	fclose(p_fp);
-
-	fwrite(&data_size, 1, 4, star_fp);
-
-	d_fp = fopen(data_name, "rb");
-	buf_size = 0;
-	do {
-		char buf[512];
-		buf_size = fread(buf, 1, 512, d_fp);
-		fwrite(buf, 1, buf_size, star_fp);
-	} while(buf_size > 0);
-	fclose(d_fp);
-
+	archive_path(target_dir, basename(target_dir), "", star_fp);
 	fclose(star_fp);
-	remove(data_name);
-	remove(path_name);
-	
-	printf("ARCHIVE: %s (file_no: %u, data-size: %u)\n", star_path, file_no, data_size);
+
+	printf("ARCHIVE: %s (file_no: %d, data-size: %u)\n", star_path, file_no, data_size);
 
 }
 
 void
-archive_path (char * target_dir, char * des_dir, char * sub_dir, FILE * p_fp, FILE * d_fp) {
+archive_path (char * target_dir, char * des_dir, char * sub_dir, FILE * s_fp) {
 
 	char * inner_dir_path;
 
@@ -120,7 +90,7 @@ archive_path (char * target_dir, char * des_dir, char * sub_dir, FILE * p_fp, FI
 			}
 
 			char * origin_path, * achiv_path;
-			int origin_path_len, achiv_path_len; 
+			unsigned int origin_path_len, achiv_path_len; 
 
 			origin_path_len = strlen(target_dir)+strlen(sub_child_dir)+2;
 			achiv_path_len = strlen(des_dir)+strlen(sub_child_dir)+3;
@@ -133,17 +103,19 @@ archive_path (char * target_dir, char * des_dir, char * sub_dir, FILE * p_fp, FI
 
                         if (ep->d_type == DT_DIR) {
 
-				archive_path(origin_path, achiv_path, sub_dir, p_fp, d_fp);
+				archive_path(origin_path, achiv_path, sub_dir, s_fp);
                         } 
 			else {
 
-				int b_size = fwrite(strcat(achiv_path, ";"), 1, strlen(achiv_path)+1, p_fp);
-				file_no += b_size;
-				if (file_no > (UINT_MAX)) {
-					fprintf(stderr, "too many files. (%d)\n", file_no);
-					exit(1);
-				}
+				// (1) file path
+				achiv_path_len = strlen(achiv_path);
+				int b_size;
+				b_size = fwrite(&achiv_path_len, 1, 4, s_fp);
+				b_size = fwrite(achiv_path, 1, achiv_path_len, s_fp);
 
+				file_no ++;
+
+				// (2) file data
 				FILE * fp = fopen(origin_path, "rb");
 				int fd = fileno(fp);
         			struct stat _stat;
@@ -151,11 +123,14 @@ archive_path (char * target_dir, char * des_dir, char * sub_dir, FILE * p_fp, FI
 				off_t f_size = _stat.st_size;
 				data_size += f_size;
 
+				unsigned int uf_size = (unsigned int) f_size;
+				b_size = fwrite(&uf_size, 1, 4, s_fp);
+
 				b_size = 0;
 				do {
 					char buf[512];
 					b_size = fread(buf, 1, 512, fp);
-					b_size = fwrite(buf, 1, b_size, d_fp);
+					b_size = fwrite(buf, 1, b_size, s_fp);
 
 				} while (b_size > 0);
 
@@ -176,92 +151,148 @@ archive_path (char * target_dir, char * des_dir, char * sub_dir, FILE * p_fp, FI
         }
 
 	free(inner_dir_path);
-
 }
 
 void
 list (char * star_path) {
-	
 	if (access(star_path, F_OK) == 1) {
 		fprintf(stderr, "target star file not exists\n");
 		exit(1);
 	}
 
 	FILE * s_fp = fopen(star_path, "rb");
+	do {	
+		printf("roof\n");
+		unsigned int path_n;
+		int b_size = fread(&path_n, 1, 4, s_fp);
+		if (b_size != 4) {
+			break;
+		}
 	
-	unsigned int path_n;
-	fread(&path_n, 1, 4, s_fp);
-
-	int b_size = 0;
-
-	do {
-		char buf[512];
-		if (path_n > 512) {
-			b_size = fread(buf, 1, 512, s_fp);
-			path_n -= b_size;
-		}
-		else {
+		char * file_path = malloc(path_n * sizeof(char));
+		// strcpy(file_path, "");
+		do {
+			char buf[512];
 			b_size = fread(buf, 1, path_n, s_fp);
+			strncat(file_path, buf, b_size);
 			path_n -= b_size;
-		}
 
-		int b_b = 0;
-		while (b_b < b_size) {
-			if(buf[b_b] == ';') {
-				printf("\n");
+		} while (path_n > 0);
+
+		unsigned int data_s;
+		b_size = fread(&data_s, 1, 4, s_fp);
+		if (b_size != 4) {
+			fprintf(stderr, "cannot read data with %s\n", file_path);
+			exit(1);
+		}
+	
+		printf("%s data size: %u\n", file_path, data_s);
+		do {
+			char buf[512];
+			if (data_s > 512) {
+				b_size = fread(buf, 1, 512, s_fp);
 			}
 			else {
-				printf("%c", buf[b_b]);
+				b_size = fread(buf, 1, data_s, s_fp);
 			}
-			b_b++;
-		}
+			data_s = data_s - ((unsigned int)b_size);
+		} while (data_s > 0);
+		free(file_path);
 
-	} while (path_n > 0);
+	} while (1);
 
 	fclose(s_fp);
+
 }
 
 void
 extract (char * star_path) {
-	/*
+/*
 	if (access(star_path, F_OK) == 1) {
 		fprintf(stderr, "target star file not exists\n");
 		exit(1);
 	}
 
-	char * path_name = ".path";
-	FILE * p_fp = fopen(path_name, "wb");
-
 	FILE * s_fp = fopen(star_path, "rb");
+	do {	
+		unsigned int path_n;
+		int b_size = fread(&path_n, 1, 4, s_fp);
+		if (b_size < 1) {
+			break;
+		}
 	
-	unsigned int path_n;
-	fread(&path_n, 1, 4, s_fp);
-	fwrite(&path_n, 1, 4, p_fp);
+		char * file_path = malloc(path_n * sizeof(char));
+		do {
+			char buf[512];
+			b_size = fread(buf, 1, path_n, s_fp);
+			strncat(file_path, buf, b_size);
+			path_n -= b_size;
+			
+		} while (path_n > 0);
 
-	int b_size = 0;
-	do {
-		char buf[512];
-		b_size = fread(buf, 1, 512, s_fp);
-		b_size = fwrite(buf, 1, b_size, p_fp);
-		path_n -= b_size;
 
-	} while (path_n > 0);
-	fclose(p_fp);
+		unsigned int data_s;
+		b_size = fread(&data_s, 1, 4, s_fp);
+		if (b_size < 1) {
+			fprintf(stderr, "cannot read data with %s\n", file_path);
+			exit(1);
+		}
+	
+		char * untar_fpath = malloc((strlen(star_path) + 1 +path_n) * sizeof(char));
+		sprintf(untar_fpath, "%s/%s", star_path, file_path);
 
-	unsigned int data_size;
-	fread(&data_size, 1, 4, s_fp);
-
-	do {
-		char buf[512];
-		b_size = fread(buf, 1, 512, s_fp);
-
+		// mkdirs()
+		char * parent_dir_path = parent_dir(untar_fpath);
+		if (! mkdirs(parent_dir_path)) {
+			fprintf(stderr, "mkdirs() failed\n");
+			exit(1);
+		}
+		free(parent_dir_path);
 		
+	
+		FILE * fp = fopen(untar_fpath, "wb");
+		do {
+			char buf[512];
+			b_size = fread(buf, 1, 512, s_fp);
+			b_size = fwrite(buf, 1, b_size, fp);
+			data_s -= b_size;
+			
+		} while (data_s > 0);
+		fclose(fp);
 
-		data_size -= b_size;
+		free(untar_fpath);
+		free(file_path);
 
-	} while (data_size > 0);
+	} while (1);
 
 	fclose(s_fp);
 	*/
 }
 
+int mkdirs(char * dir_path) {
+
+	char * p_dir;
+	if (access(dir_path, F_OK) == 0) {
+		/* already exists*/
+		return 0;
+	}
+	else if (access((p_dir=parent_dir(dir_path)), F_OK) == 0) {
+		free(p_dir);
+		return mkdir(dir_path, S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) == -1;
+	}
+	else {
+		
+		int val =  mkdirs(p_dir);
+		free(p_dir);
+		return val;
+	}
+}
+
+char * parent_dir(char * path) {
+
+	int lidx = strrchr(path, '/') - path;
+	char * parent_dir_path = malloc(lidx * sizeof(char));
+	strncpy(parent_dir_path, path, lidx);
+	
+	return parent_dir_path;
+}
